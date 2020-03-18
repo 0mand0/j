9f0a3a8884161fc1c0d003996234844af179f58e
0mand0 <mandalorian@outlook.my>
2020-03-18 22:50:41 +0100

    add Hi Earth!

---
+++ HiEarth.java
@@ -0,0 +1,11 @@
+/*
+* Copyright (c) 2020, Mando (0mand0) and/or Samuel Mofrad. All rights reserved. 
+*/
+
+class HiEarth {
+    public static void main (String[] args) {
+        System.out.println("Hi, Earth,"
+        + "\n"
+        + "Hello everybody!");
+    }
+}
